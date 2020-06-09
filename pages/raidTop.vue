9a46e99389fbc9a9d008b408f164db1f7d9008bf
i.bikmaev <koshakk5@gmail.com>
2020-06-09 15:35:48 +0500

    Add edu task

---
+++ pages/raidTop.vue
@@ -0,0 +1,55 @@
+<template>
+  <a-table :columns="columns" :data-source="raid_top">
+      <span slot="fractions" slot-scope="fraction">{{fraction === 'horde' ? 'Орда' : 'Альянс'}}</span>
+  </a-table>
+</template>
+
+<script>
+import raidTop from '~/apollo/queries/fetchRaidTop.gql';
+
+const columns = [
+    {
+        title: 'Место',
+        dataIndex: 'place',
+        key: 'place',
+    },
+    {
+        title: 'Гильдия',
+        dataIndex: 'guildName',
+        key: 'guildName',
+    },
+    {
+        title: 'Сторона',
+        dataIndex: 'fraction',
+        scopedSlots: { customRender: 'fractions'},
+    },
+    {
+        title: 'Сервер',
+        dataIndex: 'realm',
+        key: 'realm',
+    },
+    {
+        title: 'Прогресс',
+        dataIndex: 'progress',
+        key: 'progress',
+    },
+]
+
+export default {
+    apollo: {
+        raid_top: {
+            prefetch: false,
+            query: raidTop,
+        }
+    },
+    data() {
+        return {
+            columns
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
